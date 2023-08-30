504dc39cfd731316d7ab9142ae03e819bb9182ff
vap1 <vaibhavvasant1705@gmail.com>
2023-08-30 14:29:16 +0530

    First Commit

--- src/types/Types.ts
+++ src/types/Types.ts
@@ -1,18 +1,40 @@
 
-export interface HomepageData {
+interface HomepageData {
   id: string;
   title: string;
   description: string;
   lastUpdated: number;
 }
 
-export interface Metadata {
+interface Metadata {
   metadataId: string;
   userId: string;
   userName: string;
   userAge: number;
   userLocation: string;
-  userPreferences: { [key: string]: string };
+  userPreferences: Record<string, string>;
   createdAt: number;
   updatedAt: number;
 }
+
+interface UpdateMetadataDisplayRequest {
+  searchCriteria: string;
+  filters: string[];
+  sortOption: string;
+}
+
+interface NavigateMetadataPagesRequest {
+  pageNumber: number;
+}
+
+interface FetchDetailedMetadataRequest {
+  metadataId: string;
+}
+
+export type {
+  HomepageData,
+  Metadata,
+  UpdateMetadataDisplayRequest,
+  NavigateMetadataPagesRequest,
+  FetchDetailedMetadataRequest,
+};
